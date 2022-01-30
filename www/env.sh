d755790fc99c0b14b193ca6c03a8b4771fd1ecac
Keisuke Kubota <Kk.bb1130@outlook.jp>
2022-01-30 12:47:09 +0900

    Add. www

---
+++ www/env.sh
@@ -0,0 +1,3 @@
+# this command might be needed if you had some trouble with 'npm run start'
+# for more details: visit here https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported
+$env:NODE_OPTIONS="--openssl-legacy-provider"
